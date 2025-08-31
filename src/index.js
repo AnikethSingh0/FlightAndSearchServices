9d475ba0d2a4dbfe83b2c72e23b9c2e6fee206c3
Aniketh Singh <aniketh9410280466@gmail.com>
2025-09-01 01:37:27 +0530

    Setup database with sequelize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express=require("express");
-const {PORT}=require('./config/serverConfig')
+
+const {PORT}=require('./config/serverConfig');
+const { urlencoded } = require("body-parser");
 
 
 const setupAndStartServer=async()=>{
     const app=express();
+
+    app.use(express.json());
+    app.use(urlencoded({extended:true}))
     app.listen(PORT,()=>{
-    console.log(`${PORT} has started..`)
+    console.log(`${PORT} has started..`);
     })
 
 }
